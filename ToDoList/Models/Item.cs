9097770112fdf9b046fe5f2b73f13affd4bfc3bb
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-06-26 16:29:38 -0700

    baseline To-Do List app functional

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System;
+
+namespace ToDoList.Models
+
+{
+  public class Item
+  {
+    public string Description { get; set; }
+    public int UniqueId { get; set; }
+
+    private static List<Item> _instances = new List<Item> {};
+
+    public static List<Item> GetAll()
+      {
+        return _instances;
+      }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static int ID = 1;
+    public static int CountItems()
+    {
+      return ID++;
+    }
+      // count = 1;
+      // return count;
+      // count ++;
+
+      // for(int i = 1; i < 10; i++)
+      // {
+      //   return i;
+      // };
+     
+    //  public static int getID(){    return ID++;}
+
+    // }
+
+    public Item(string description)
+    {
+      Description = description;
+      //field for unique identifier? that gives it # and then increments it
+      UniqueId = CountItems();
+      _instances.Add(this);
+    }
+
+    
+
+
+  }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      // Console.WriteLine("Welcome to the To-Do List");
+      
+      Console.WriteLine("Would you like to add an item to your To-Do List? ['Y' for yes, 'Enter' for no]");
+      string answer = Console.ReadLine();
+
+      
+      if (answer == "Y" || answer == "y")
+      {
+          Console.WriteLine("Enter your to-do item: ");
+          Item item = new Item(Console.ReadLine());
+          Console.WriteLine(item.Description + " has been added to your To-Do List!");
+          
+              
+          Main();
+      } 
+      else 
+      { 
+        //ask if they want to view list or exit program
+        Console.WriteLine("Do you want to view your list so far or exit the program? ['Y' for view or 'Enter' for exiting]");
+        string viewOrExit = Console.ReadLine();
+        if (viewOrExit == "Y" || viewOrExit == "y")
+        { 
+          List<Item> result = Item.GetAll();
+          // Console.WriteLine(result);
+            Console.WriteLine("Your to do list:");
+          foreach (Item thisItem in result)
+          {
+            Console.WriteLine(thisItem.UniqueId + ": " + thisItem.Description);
+          }
+
+          Main();
+        }
+        else
+        {
+          Console.WriteLine("Goodbye!");
+        }
+      }
+
+
+    }
+  }
+}
